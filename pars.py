7de0832b69a577e8d635f74cd93f14a303c8b645
Dez944 <batyunya.mariya@gmail.com>
2024-10-09 17:02:54 +0300

    Add files via upload

--- pars.py
+++ pars.py
@@ -4,15 +4,16 @@ tsts = []
 
 with open('4242.txt') as tlog:
     for i in tlog:
+        if 'Test  :' in i:
+            print(tst := (i[i.find('Test  :'):])[8:43].strip())
+            if tst not in tsts:
+                tsts.append(tst[0:43])
         if 'Result:' in i:
             print(res := (i[i.find('Result:'):])[0:15])
         if (d := 'Date:') in i:
             d = str(i[i.find(d):])
             dt = mes.index(d1 := d[9:12])+1
             print(f'{d[:8]}.{dt}.{d[13:]}')
-        if 'Test  :' in i:
-            print(tst := (i[i.find('Test  :'):])[0:43].strip())
-            tsts.append(tst[8:43])
+
 
 print(tsts)
-
