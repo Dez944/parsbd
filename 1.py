39ff142c2869b494c2c16934f8e20e5a6d0fe9be
Dez944 <batyunya.mariya@gmail.com>
2024-10-18 00:46:43 +0300

    Delete 1.py

--- 1.py
+++
@@ -1,6 +0,0 @@
-t = ['TRANSCEIVER FLATNESS', 'TRANSCEIVER FLATNESS', 'TRANSCEIVER LEVEL', 'TRANSCEIVER NOISE']
-d = ['Date: 30.3.2007 17:54:07.31', 'Date: 30.3.2007 17:55:20.79', 'Date: 30.3.2007 17:55:23.75', 'Date: 30.3.2007 17:55:31.46']
-r = ['Result: Stoppe', 'Result: Passed', 'Result: Passed', 'Result: Passed']
-
-a = list(zip(t, d, r))
-print(a[0])
